e71e5a9626c0d7b1b1c4df86ca4bb0995be4e6ab
MaravixG <ellagoode9@gmail.com>
2024-02-23 11:47:30 -0500

    Initial Commit

---
+++ PortalHackathon/PortalHackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortalHackathonApp.swift
+//  PortalHackathon
+//
+//  Created by Ella Goode on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PortalHackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
